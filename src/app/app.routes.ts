bb68aa02ce0c37b18cfddcd95a6041054ebb3ef3
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2026-01-11 17:16:59 +0100

    Updated website navbar styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {HomePageComponent} from "./home-page/home-page.component";
-import {ButtonsComponentsComponent} from "./home-page/buttons-components/buttons-components.component";
+import {ComponentsPageComponent} from "./home-page/components-page/components-page.component";
+import {TemplatesPageComponent} from "./home-page/templates-page/templates-page.component";
+import {HelpGuideComponent} from "./home-page/help-guide/help-guide.component";
 
 export const routes: Routes = [
   {
@@ -9,9 +11,19 @@ export const routes: Routes = [
     title: 'Kerfais library',
     children: [
       {
-        path: 'buttons',
-        component: ButtonsComponentsComponent,
-        title: 'Kerfais library',
+        path: 'components',
+        component: ComponentsPageComponent,
+        title: 'Components',
+      },
+      {
+        path: 'templates',
+        component: TemplatesPageComponent,
+        title: 'Templates'
+      },
+      {
+        path: 'help',
+        component: HelpGuideComponent,
+        title: 'Help guide'
       }
     ]
   }
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
